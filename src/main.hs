0130aa5c9d12f4aa06a6e663f6cba2ec2db0700e
cleissonbarbosa <cleisson147@gmail.com>
2023-09-22 20:37:17 -0300

    feat: support .rinha

--- src/main.hs
+++ src/main.hs
@@ -3,8 +3,10 @@ module Main
     ) where
 
 import Data.Aeson ( decode )
+import Data.List (isSuffixOf)
 import qualified Data.ByteString.Lazy.Char8 as B
 import System.Environment ( getArgs )
+import System.Process (readProcess)
 import Parse ()
 import Eval ( interpret )
 
@@ -13,8 +15,15 @@ main = do
   args <- getArgs
   case args of
     [filename] -> do
-      json <- B.readFile filename
-      case decode json of
-        Just ast -> interpret ast >> return ()
-        Nothing -> putStrLn "Failed to parse JSON"
+      if ".rinha" `isSuffixOf` filename
+        then do
+          rinhaJson <- readProcess "./lib/bin/rinha" [filename] ""
+          case decode (B.pack rinhaJson) of
+            Just ast -> interpret ast >> return ()
+            Nothing -> putStrLn "Failed to parse Rinha"
+        else do
+          json <- B.readFile filename
+          case decode json of
+            Just ast -> interpret ast >> return ()
+            Nothing -> putStrLn "Failed to parse JSON"
     _ -> putStrLn "Usage: program <filename>"
